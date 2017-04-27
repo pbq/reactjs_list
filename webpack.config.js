15162c94f873120de4940a9bf45828aa7b5b52f7
pbq <pierre.bauquin+git@gmail.com>
2017-04-27 17:51:14 +0200

    modification de l'acces à la page index.html

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,24 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,// a regular expression that catches .jsx files
-        include : APP_DIR,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?/, // a regular expression that catches .jsx files
+            include: APP_DIR,
+            loader: 'babel-loader'
+        }]
+    },
+    devServer: {
+        contentBase: "./src/client",
+        hot: true,
+        port: 3000,
+        historyApiFallback: true
+    },
 };
 
 module.exports = config;
