a4b97dd51000803f2e3b8abcfaf08d1eacdcb9bf
pbq <Pierre@MacBook-Pro.local>
2017-04-27 19:26:48 +0200

    ajout de todomvc

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const webpack = require('webpack');
 const path = require('path');
 
@@ -9,9 +10,30 @@ var config = {
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
+=======
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname , "dist");
+var SRC_DIR = path.resolve(__dirname , "src");
+
+process.traceDeprecation = false;
+
+module.exports = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + "/app",
+        // if the above line does not work, try `path: __dirname + '/build'`
+        filename: "bundle.js",
+        publicPath:"/app/"
+>>>>>>> Stashed changes
     },
+    resolveLoader: {
+  modules: [path.resolve(__dirname, "src"), "node_modules"]
+},
     module: {
         loaders: [{
+<<<<<<< Updated upstream
             test: /\.jsx?/, // a regular expression that catches .jsx files
             include: APP_DIR,
             loader: 'babel-loader'
@@ -26,3 +48,20 @@ var config = {
 };
 
 module.exports = config;
+=======
+            test: /\.js?/, // a regular expression that catches .js files
+            include: SRC_DIR,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets:["react", "es2015"]
+            }
+        }]
+    },
+    devServer: {
+        port: 3000, // most common port
+        contentBase: "src/",
+        inline: true
+    }
+}
+>>>>>>> Stashed changes
