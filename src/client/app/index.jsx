11bede9a19506931250ce4fdd2a0e3c2e1be5c6b
pbq <pierre.bauquin+git@gmail.com>
2017-04-27 17:47:00 +0200

    modification du code jsx

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import OnLikeComponent from './onLikeComponent.jsx';
 
 class App extends React.Component {
   render () {
-    return <h1> Hello React!</h1>;
+    return (
+      <div>
+        <h1> Hello React!</h1>
+        <OnLikeComponent />
+      </div>
+      );
   }
 }
 
