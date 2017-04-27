c1dee97ec612c6cca2b8bf3e0c87ae1dfaab5f03
pbq <pierre.bauquin+git@gmail.com>
2017-04-27 17:45:54 +0200

    modification de la conf du webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
