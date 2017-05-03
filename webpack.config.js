64db486e10c50d067e1c73c80b13607c2fe9c327
pbq <pierre.bauquin+git@gmail.com>
2017-05-03 15:21:59 +0200

    ajout du extract-text-webpack-plugin  pour extraire le css du bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+/*
+It moves all the required *.css modules in entry chunks into a separate CSS file.
+So your styles are no longer inlined into the JS bundle, but in a separate CSS file (styles.css).
+If your total stylesheet volume is big,
+it will be faster because the CSS bundle is loaded in parallel to the JS bundle.
+*/
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
@@ -25,30 +32,19 @@ var config = {
                 loader: "babel-loader"
             },
             {
-              test: /\.css$/, // a regular expression that catches .css 
-              use: [{
-                loader: "style-loader"
-              },{
-                loader: "css-loader",
-                options: {
-                  // Enable/Disable CSS Modules
-                  modules: true,
-
-                   // an option in order to be more readable
-                   // name=App local=heading -  6 first caracters of hash
-                  localIdentName: '[name]-[local]-[hash:base64:6]',
-
-                  // Export Classnames in CamelCase => transform css class xxx-xxx in xxxXxx
-                  camelCase: true
-                }
-              }]
+                test: /\.css$/, // a regular expression that catches .css
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader', // loader(e.g 'style-loader') that should be used when the CSS is not extracted (i.e. in an additional chunk when allChunks: false)
+                    use: 'css-loader?modules,localIdentName=[name]-[local]-[hash:base64:6]'
+                })
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-          template: "src/client/index.html"
+            template: "src/client/index.html"
         }),
+        new ExtractTextPlugin("styles.css"),
         new DashboardPlugin()
     ]
 };
